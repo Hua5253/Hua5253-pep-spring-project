d4e2b684e9aca0063281af79ca7524ee6a542f2b
Hua5253 Liu <liuzhihua213@gmail.com>
2024-07-30 18:33:28 +0000

    add default constructor of MessageDTO

--- src/main/java/com/example/entity/MessageDTO.java
+++ src/main/java/com/example/entity/MessageDTO.java
@@ -3,6 +3,8 @@ package com.example.entity;
 public class MessageDTO {
     private String messageText;
 
+    public MessageDTO() {}
+
     public MessageDTO(String messageText) {
         this.messageText = messageText;
     }
